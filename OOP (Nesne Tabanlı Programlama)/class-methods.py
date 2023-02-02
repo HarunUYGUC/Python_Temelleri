abb949d483b8674eadd04eed2e80897bbfacf8a5
Harun Uyguç <harun.uyguc@hotmail.com>
2023-02-02 17:47:07 +0300

    Update

--- OOP (Nesne Tabanlı Programlama)/class-methods.py
+++ OOP (Nesne Tabanlı Programlama)/class-methods.py
@@ -6,23 +6,19 @@ class User:
     def display_active_users(cls):
         return f"{cls.active_users} tane aktif kullanıcı var."
 
+    # Instance Method yerine 
+    # Class Method kullanarak aşağıdaki şekilde de nesne oluşturabiliriz.
     @classmethod
     def from_string(cls, data_str):
         firstName, lastName, age = data_str.split(",")
         return cls(firstName, lastName, age)
 
+    # Instance Method kullanarak aşağıdaki şekilde nesne oluşturabiliriz.
     def __init__(self, _firstName, _lastName, _age):
         self.firstName = _firstName
         self.lastName = _lastName
         self.age = _age
         User.active_users += 1 
-    
-    def full_name(self):
-        return f"{self.firstName} {self.lastName}"
-
-    def log_out(self):
-        User.active_users -= 1
-        return f"{self.full_name()} uygulamadan çıkış yaptı!"
 
 user = User("Harun", "Uyguç", 19)
 print(user.firstName)
