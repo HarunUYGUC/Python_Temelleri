77b4816766448aa86d2bf8eeaf1557e03fece517
Harun Uyguç <harun.uyguc@hotmail.com>
2023-08-22 21:33:57 +0300

    Speed Test

---
+++ Iterators & Generators/speed-test.py
@@ -0,0 +1,23 @@
+import time
+
+def speed_test(fn):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        print(f"{fn.__name__} metotu çalışıyor...")
+        result = fn(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Geçen süre: {run_time}")
+        return result
+    return wrapper
+
+@speed_test
+def sum_gen():
+    return sum((x for x in range(10000000)))
+
+@speed_test
+def sum_list():
+    return sum([x for x in range(10000000)])
+
+print(sum_gen())
+print(sum_list())
