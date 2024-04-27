eb3d6ca8fc321519f9c055b87a6087d18a64d992
Harun Uyguç <harun.uyguc@hotmail.com>
2024-04-27 13:46:32 +0300

    Creating Connections

---
+++ NoSQL - MongoDB/creating-collections.py
@@ -0,0 +1,55 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017")
+
+mydb = myclient["node-app"]
+mycollection = mydb["products"]
+
+print(mydb.list_collection_names())
+
+
+product = {"name": "Samsung S5", "price": 2000}
+
+result = mycollection.insert_one(product)
+
+print(result)
+print(type(result))
+print(result.inserted_id)
+
+
+productList = [
+    {"name": "Samsung S6", "price": 3000},
+    {"name": "Samsung S7", "price": 4000},
+    {"name": "Samsung S8", "price": 5000},
+    {"name": "Samsung S9", "price": 6000},
+    {"name": "Samsung S10", "price": 7000},
+    {"name": "Samsung S11", "price": 8000}
+]
+
+result = mycollection.insert_many(productList)
+
+print(result.inserted_ids)
+
+
+productList = [
+    {"_id": 1, "name": "Samsung S6", "price": 3000, "description": "iyi telefon"},
+    {"_id": 2, "name": "Samsung S7", "price": 4000, "categories": ["telefon", "elektronik"]},
+    {"_id": 3, "name": "Samsung S8", "price": 5000},
+    {"_id": 4, "name": "Samsung S9", "price": 6000},
+    {"_id": 5, "name": "Samsung S10", "price": 7000},
+    {"_id": 6, "name": "Samsung S11", "price": 8000}
+]
+
+result = mycollection.insert_many(productList)
+
+print(result.inserted_ids)
+
+
+productList = [
+    {"name": "Samsung S6", "price": 3000, "description": "iyi telefon"},
+    {"name": "Samsung S7", "price": 4000, "categories": ["telefon", "elektronik"]}
+]
+
+result = mycollection.insert_many(productList)
+
+print(result.inserted_ids)
