290bf23cef67cc036a3d7e56beee1a823dc9eb10
Harun Uyguç <harun.uyguc@hotmail.com>
2024-07-14 14:54:09 +0300

    Pandas Frames

---
+++ Veri Analizi - Pandas/pandas-frames.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+s1 = pd.Series([3, 2, 0, 1])
+s2 = pd.Series([0, 3, 7, 2])
+ 
+data = dict(apples = s1, oranges = s2)
+
+df = pd.DataFrame(data)
+
+print(df)
+
+data = [["Ahmet", 50], ["Ali", 60], ["Yağmur", 70], ["Çınar", 80]]
+data2 = {"Name": ["Ahmet", "Ali", "Yağmur", "Çınar"], "Grade": [50, 60, 70, 80]}
+dict_list = [
+                {"Name": "Ahmet", "Grade": 50},
+                {"Name": "Ali", "Grade": 60},
+                {"Name": "Yağmur", "Grade": 70},
+                {"Name": "Çınar", "Grade": 80}
+            ]
+
+# df = pd.DataFrame()
+# df = pd.DataFrame([1, 2, 3, 4])
+# df = pd.DataFrame([["Ahmet", 50], ["Ali", 60], ["Yağmur", 70], ["Çınar", 80]])
+# df = pd.DataFrame(data, columns = ["Name", "Grade"])
+# df = pd.DataFrame(data, columns = ["Name", "Grade"], index = [1, 2, 3, 4])
+# df = pd.DataFrame(data, [1, 2, 3, 4], ["Name", "Grade"])
+# df = pd.DataFrame(data, index = [1, 2, 3, 4], columns = ["Name", "Grade"])
+# df = pd.DataFrame(data, index = [1, 2, 3, 4], columns = ["Name", "Grade"], dtype = float) # Bende hata verdi!
+# df = pd.DataFrame(data2)
+# df = pd.DataFrame(data2, index = ["212", "232", "236", "456"])
+# df = pd.DataFrame(dict_list)
+df = pd.DataFrame(dict_list, index = ["212", "232", "236", "456"])
+
+print(df)
